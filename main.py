448aa9c06d360afabf39d65ccb8e548b50c9c5d4
massidiqfattah <massidiqfattah@gmail.com>
2021-04-09 23:34:03 +0700

    button

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import streamlit as st
 
 st.title('Fuzzy Logic Implementasi')
-col1, col2, col3 = st.beta_columns(3)
+col1, col2, col3 = st.beta_columns([.1,.1,.1])
 
-home = st.button("Home","Home")
-fungsi = st.button("Fungsi","Fungsi")
-fuzzy = st.button("Fuzzy","Fuzzy")
+home = col1.button("Home","Home")
+fungsi = col2.button("Fungsi","Fungsi")
+fuzzy = col3.button("Fuzzy","Fuzzy")
 
 if home:
-    st.subheader('Nama : Muhammad Assidiq Fattah')
-    st.subheader('NRP : 152017015')
+    col1, col2 = st.beta_columns([1,1])
+    col1.subheader('Nama : Muhammad Assidiq Fattah')
+    col1.subheader('NRP : 152017015')
+    col2.subheader('Nama : Syahrul Fathurahman Erawan')
+    col2.subheader('NRP : 152017030')
 elif fungsi:
     st.subheader('Fungsi Fuzzy Logic')
+elif fuzzy:
+    st.subheader('Implementasi Fuzzy Logic')
