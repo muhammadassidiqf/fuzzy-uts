3ad371aeea4965388cb0436ede35807e6939dc7c
sferawann <sferawann@github.com>
2021-04-11 15:41:23 +0700

    case

--- main.py
+++ main.py
@@ -5,30 +5,17 @@ import numpy as np
 st.set_page_config(layout="wide")
 st.title('Fuzzy Logic Implementasi')
 
-# home = col1.button("Home","Home")
-# fungsi = col2.button("Fungsi","Fungsi")
-# fuzzy = col3.button("Fuzzy","Fuzzy")
-
-# if home:
-#     col1, col2 = st.beta_columns([1,1])
-#     col1.subheader('Nama : Muhammad Assidiq Fattah')
-#     col1.subheader('NRP : 152017015')
-#     col2.subheader('Nama : Syahrul Fathurahman Erawan')
-#     col2.subheader('NRP : 152017030')
-# elif fungsi:
-#     st.subheader('Fungsi Fuzzy Logic')
-# elif fuzzy:
-#     st.subheader('Implementasi Fuzzy Logic')
 out_name_a=[]
 var_name_a=[]
 sat_name_a=[] 
 jika1_name=[] 
 jika2_name=[] 
 def himp(e,r):
-    co1, co2, co3, co4 = st.beta_columns([.5,.5,.5,.5])
+    co1, co2, co3, co4,co5 = st.beta_columns([.5,.5,.5,.5,.5])
     # himp_num_a=[] 
     nilai_min = 0
     nilai_max = 0
+    nilai_case = 0
     istilah_min = ''
     istilah_max = ''
     for i in range(int(e)):
@@ -36,8 +23,9 @@ def himp(e,r):
         istilah_min = co2.text_input('Istilah Minimum '+str(r)+' '+str(i+1))
         nilai_max = co3.number_input('Nilai Maximum '+str(r)+' '+str(i+1), min_value=0)
         istilah_max = co4.text_input('Istilah Maximum '+str(r)+' '+str(i+1))
+        nilai_case = co5.number_input('Nilai Case '+str(r)+' '+str(i+1), min_value=0)
         # himp_num_a.append(((nilai_min,istilah_min),(nilai_max,istilah_max)))
-    return ((nilai_min,istilah_min),(nilai_max,istilah_max))
+    return ((nilai_min,istilah_min),(nilai_max,istilah_max),(nilai_case))
 
 
 def get(a):
@@ -52,19 +40,16 @@ def get2(a):
     return jika2_name
 
 var_number = st.number_input('Masukan jumlah Variabel', min_value=0, max_value=10)
-col1, col2, col3, col4 = st.beta_columns([.5,.5,.5,.6])
+col1, col2, col3, col4,col5 = st.beta_columns([.5,.5,.5,.5,.6])
 for i in range(int(var_number)):
     out_name = col1.selectbox('Tipe Variabel '+str(i+1),('Output ','Input '))
     var_name = col2.text_input('Nama Variabel '+str(i+1))
     sat_name = col3.text_input('Satuan '+str(i+1))
     himp_num = col4.number_input('Masukan Jumlah Himpunan '+str(i+1), min_value=0, max_value=10)
+    case_num = col5.number_input('Masukan Jumlah Case '+str(i+1), min_value=0, max_value=10)
     a = himp(himp_num, var_name)
-    out_name_a.append(((out_name, var_name, sat_name, a)))
+    out_name_a.append((((out_name, var_name, sat_name, a, case_num))))
    
-    # var_name_a.append((out_name_a[i], himp(himp_num)))
-    # sat_name_a.append(sat_name)
-    # b = var_name_a.append(himp_num)
-    # himp(b,a)
 
 rule_num = st.number_input('Masukan jumlah rules', min_value=0, max_value=10)
 for i in range (int(rule_num)):
@@ -82,14 +67,4 @@ st.write(out_name_a)
 
 for i in out_name_a:    
     st.write(i[0])
-    # for j in i[3]:
-    #     for k in j[1]:
-    #         st.write(k)
-    
-# var_name_ab = np.array([var_name_a])  
-# col2.write('Tabel Variabel Name')
-# col2.write(var_name_a)
-# sat_name_ab = np.array([sat_name_a])  
-# col3.write('Tabel Satuan Name')
-# col3.write(sat_name_a)
-# col4.write(himp_num_a)
+   
