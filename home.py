56a7847c96c98549190a1e16ee30966479300810
sferawann <sferawann@github.com>
2021-04-14 15:28:38 +0700

    home-teori

---
+++ home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('UTS Pemrograman Simulai')
+st.title('Fuzzy Mamdani')
+st.subheader('Nama : Muhammad Assidiq Fattah')
+st.subheader('NRP : 152017015')
+st.subheader('Nama : Syahrul Fathurrahman Erawan')
+st.subheader('NRP : 152017030')
